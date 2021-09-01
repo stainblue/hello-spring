77f12cde7a0d41d1e9031c09c31b4f1179b923a7
Doin Cha <stainblue1129@gmail.com>
2021-09-01 19:34:03 +0900

    test: order, discount

---
+++ src/test/java/hello/hellospring/order/OrderServiceTest.java
@@ -0,0 +1,28 @@
+package hello.hellospring.order;
+
+import hello.hellospring.member.Grade;
+import hello.hellospring.member.Member;
+import hello.hellospring.member.MemberService;
+import hello.hellospring.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        // given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        // when
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        // that
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
