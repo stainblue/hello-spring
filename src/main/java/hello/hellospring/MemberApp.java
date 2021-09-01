6bed3b74c4cca32546f2903fdb23084955590cd8
Doin Cha <stainblue1129@gmail.com>
2021-09-01 19:08:43 +0900

    test: member

---
+++ src/main/java/hello/hellospring/MemberApp.java
@@ -0,0 +1,18 @@
+package hello.hellospring;
+
+import hello.hellospring.member.Grade;
+import hello.hellospring.member.Member;
+import hello.hellospring.member.MemberService;
+import hello.hellospring.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
