5178581aace3ff2b344c3b3a62bbd628e290ea73
Doin Cha <stainblue1129@gmail.com>
2021-09-01 19:27:05 +0900

    feat: order, discount

---
+++ src/main/java/hello/hellospring/discount/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.hellospring.discount;
+
+import hello.hellospring.member.Grade;
+import hello.hellospring.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
