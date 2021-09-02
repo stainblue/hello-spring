3ac3042a5dc5dabf0b9738eb814847e423de63a5
Doin Cha <stainblue1129@gmail.com>
2021-09-02 17:29:45 +0900

    refactor: AppConfig

--- src/main/java/hello/hellospring/AppConfig.java
+++ src/main/java/hello/hellospring/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.discount.DiscountPolicy;
 import hello.hellospring.discount.FixDiscountPolicy;
 import hello.hellospring.member.MemberService;
 import hello.hellospring.member.MemberServiceImpl;
@@ -10,11 +11,18 @@ import hello.hellospring.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 }
