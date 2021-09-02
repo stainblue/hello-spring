0978ec7a6585c00038826eaa067a06f4e09fb1e6
Doin Cha <stainblue1129@gmail.com>
2021-09-02 16:24:12 +0900

    feat: rate discount policy

---
+++ src/main/java/hello/hellospring/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.hellospring.discount;
+
+import hello.hellospring.member.Grade;
+import hello.hellospring.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
