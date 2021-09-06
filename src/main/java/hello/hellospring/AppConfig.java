7481aa813f9b4c6274b874daf9c1ad7d088e6df6
Doin Cha <stainblue1129@gmail.com>
2021-09-06 21:24:42 +0900

    refactor: use spring container

--- src/main/java/hello/hellospring/AppConfig.java
+++ src/main/java/hello/hellospring/AppConfig.java
@@ -7,21 +7,28 @@ import hello.hellospring.member.MemberServiceImpl;
 import hello.hellospring.member.MemoryMemberRepository;
 import hello.hellospring.order.OrderService;
 import hello.hellospring.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
